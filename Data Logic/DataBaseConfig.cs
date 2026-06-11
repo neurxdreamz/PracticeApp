41ed4bf546762755f67b97a9a8c915e08936665f
neurxdreamz <kononovs774@mail.ru>
2026-06-11 20:40:10 +0300

    Добавил интерфейс IUserRepository

---
+++ Data Logic/DataBaseConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Logic
+{
+    public static class DataBaseConfig
+    {
+        public static string GetConnectionString()
+        {
+            return @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=деталь (для практики).accdb;Persist Security Info=False;";
+        }
+    }
+}
---
+++ Data Logic/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Data_Logic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Logic.Repositories
+{
+    public interface IUserRepository
+    {
+        User GetUserByUsername(string username);
+        IEnumerable<User> GetAllUsers();
+        void AddUser(User user, string passwordHash);
+        void DeleteUser(int userId);
+    }
+}
