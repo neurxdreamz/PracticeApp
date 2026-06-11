2479f6d7b006ceb86c20eb44275ae174fb6692b4
neurxdreamz <kononovs774@mail.ru>
2026-06-11 19:45:51 +0300

    Создал классы сущности user и role

---
+++ Data Logic/Entities/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Logic.Entities
+{
+    public class Role
+    {
+        public int IdRole { get; set; }
+        public string RoleName { get; set; }
+
+    }
+}
---
+++ Data Logic/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Logic.Entities
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public int IdRole { get; set; }
+        public string UserName { get; set; }
+
+        public Role RoleName { get; set; }
+    }
+}
