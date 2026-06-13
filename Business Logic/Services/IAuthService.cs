ee584bea21f4ab7ba2fbc647d38ae19197916e6b
neurxdreamz <kononovs774@mail.ru>
2026-06-13 15:27:09 +0300

    написал интерфейс для авторизации нового пользователя

---
+++ Business Logic/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business_Logic.Services
+{
+    public interface IAuthService
+    {
+        void RegisterNewUser (string username, int RoleId, string RawPassword);
+        bool IsUserExist(string username);
+    }
+}
