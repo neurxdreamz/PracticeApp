9857d607472e1eb2db161e1ffcee5fcb5729c1e7
neurxdreamz <kononovs774@mail.ru>
2026-06-13 16:46:25 +0300

    Создал сущность таблицы деталь

---
+++ Data Logic/Entities/Detail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Data_Logic.Entities
+{
+    public class Detail
+    {
+        
+        public int IdRecord { get; set; }
+        public string DetailName { get; set; }
+        public int BatchVolume { get; set; }
+
+      
+        public int SectorId { get; set; }
+        public int WorkerId { get; set; }
+        public int ShiftId { get; set; }
+
+        
+        public string SectorName { get; set; }
+        public string WorkerFullName { get; set; }
+        public int ShiftNumber { get; set; }
+    }
+}
