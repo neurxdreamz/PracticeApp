d4925dc6b9e9177c5c42e11a1960afe812de0533
neurxdreamz <kononovs@774gmail.com>
2026-06-23 15:57:57 +0300

    Написал сервис для таблицы деталь

---
+++ Business Logic/Services/DetailService.cs
@@ -0,0 +1,86 @@
+﻿using Data_Logic.Entities;
+using Data_Logic.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business_Logic.Services
+{
+    public class DetailService : IDetailService
+    {
+        private readonly IDetailRepository detailRepository;
+
+        
+        public DetailService(IDetailRepository DetailRepository)
+        {
+            detailRepository = DetailRepository ?? throw new ArgumentNullException(nameof(DetailRepository));
+        }
+
+        public IEnumerable<Detail> GetAllDetails()
+        {
+            return detailRepository.GetAllDetails();
+        }
+
+        public Detail GetDetailById(int id)
+        {
+            if (id <= 0)
+                throw new ArgumentException("ID детали должен быть положительным числом.", nameof(id));
+
+            return detailRepository.GetDetailById(id);
+        }
+
+        public void AddDetail(Detail detail)
+        {
+            ValidateDetail(detail); 
+            detailRepository.AddDetail(detail);
+        }
+
+        public void UpdateDetail(Detail detail)
+        {
+            if (detail == null)
+                throw new ArgumentNullException(nameof(detail));
+
+            if (detail.IdRecord <= 0)
+                throw new ArgumentException("Невозможно обновить запись: некорректный ID.", nameof(detail.IdRecord));
+
+            ValidateDetail(detail); 
+            detailRepository.UpdateDetail(detail);
+        }
+
+        public void DeleteDetail(int id)
+        {
+            if (id <= 0)
+                throw new ArgumentException("ID детали должен быть положительным числом.", nameof(id));
+
+            detailRepository.DeleteDetail(id);
+        }
+
+
+
+        /// <summary>
+        /// Централизованная проверка бизнес-правил для сущности "Деталь"
+        /// </summary>
+        private void ValidateDetail(Detail detail)
+        {
+            if (detail == null)
+                throw new ArgumentNullException(nameof(detail));
+
+          
+            if (string.IsNullOrWhiteSpace(detail.DetailName))
+                throw new ArgumentException("Название детали обязательно для заполнения.");
+
+           
+            if (detail.BatchVolume <= 0)
+                throw new ArgumentException("Объем партии должен быть больше нуля.");
+
+            if (detail.SectorId <= 0)
+                throw new ArgumentException("Необходимо выбрать участок, на котором производилась деталь.");
+
+            if (detail.WorkerId <= 0)
+                throw new ArgumentException("Необходимо назначить рабочего, ответственного за деталь.");
+
+            if (detail.ShiftId <= 0)
+                throw new ArgumentException("Необходимо указать смену.");
+        }
+    }
+}
---
+++ Business Logic/Services/IDetailService.cs
@@ -0,0 +1,16 @@
+﻿using Data_Logic.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business_Logic.Services
+{
+    public interface IDetailService
+    {
+        IEnumerable<Detail> GetAllDetails();
+        Detail GetDetailById(int id);
+        void AddDetail(Detail detail);
+        void UpdateDetail(Detail detail);
+        void DeleteDetail(int id);
+    }
+}
